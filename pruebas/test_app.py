f16f0be040ab371b9960993449fe71c6d0003d8a
Marcelo <marcelogvz27@gmail.com>
2026-04-12 16:18:11 -0300

    ejemplos pruebas unitarias

---
+++ pruebas/test_app.py
@@ -0,0 +1,14 @@
+import pytest
+from app import sumar
+
+def test_sumar():
+    assert sumar(2, 2) == 4
+
+def test_sumar_negativos():
+    assert sumar(-1, -1) == -2
+
+def test_sumar_cero():
+    assert sumar(0, 5) == 5
+
+def test_sumar_decimales():
+    assert sumar(1.5, 2.5) == 4.0
