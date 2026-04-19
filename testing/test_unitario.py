9195090a3c3930c230bc05ae2e2028ad814d274f
Marcelo <marcelogvz27@gmail.com>
2026-04-18 23:22:22 -0300

    test unitario para comprobar que funcione bien mi funcion de validacion

---
+++ testing/test_unitario.py
@@ -0,0 +1,22 @@
+#importamos la funcion pytest para realizar las pruebas unitarias
+import pytest
+
+#importo la funcion validar_mensaje para probarla
+from test_validacion import validar_mensaje
+
+def test_validar_mensaje():
+
+    #caso de prueba 1: mensaje vacio
+    assert validar_mensaje("") == False
+
+    #caso de prueba 2: mensaje con solo espacios
+    assert validar_mensaje("   ") == False
+
+    #caso de prueba 3: mensaje con mas de 200 caracteres
+    mensaje_largo = "a" * 201
+    assert validar_mensaje(mensaje_largo) == False
+
+    #caso de prueba 4: mensaje valido
+    assert validar_mensaje("Hola, este es un mensaje valido.") == True
+    
+
