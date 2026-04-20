57ef33ceb05f9f53f67605537f81e0cb326668b2
Marcelo <marcelogvz27@gmail.com>
2026-04-20 01:06:54 -0300

    llame a la funcion refactorizada y lo llame como r para poder usarlo para el testeo final

--- testing/test_unitario.py
+++ testing/test_unitario.py
@@ -2,21 +2,19 @@
 import pytest
 
 #importo la funcion validar_mensaje para probarla
-from funcion_validacion import validar_mensaje
+from funcion_validacion import ref_validar_mensaje as r
 
 def test_validar_mensaje():
 
     #caso de prueba 1: mensaje vacio
-    assert validar_mensaje("") == False
+    assert r("") == False
 
     #caso de prueba 2: mensaje con solo espacios
-    assert validar_mensaje("   ") == False
+    assert  r("   ") == False
 
     #caso de prueba 3: mensaje con mas de 200 caracteres
     mensaje_largo = "a" * 201
-    assert validar_mensaje(mensaje_largo) == False
+    assert  r(mensaje_largo) == False
 
     #caso de prueba 4: mensaje valido
-    assert validar_mensaje("Hola, este es un mensaje valido.") == True
-    
-
+    assert  r("Hola, este es un mensaje valido.") == True
